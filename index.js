c67542511ad5fe79eb37641faa41c797e59b3b81
csantiago <resolutecoder@gmail.com>
2021-08-20 17:23:52 -0500

    added ability to display jira ticket info to slack

--- index.js
+++ index.js
@@ -8,60 +8,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+dotenv.config();
+
+const bot = new SlackBot({
+  token: `${process.env.BOT_TOKEN}`,
+  name: 'storypointbot',
+});
+
 app.listen(port, () => {
   console.log(`Story point bot is listening at port ${port}`);
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/ticket/:id', async (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  const { name, pass } = basicAuth(req);
-  if (
-    name === `${process.env.STORYPOINT_USERNAME}` &&
-    pass === `${process.env.STORYPOINT_PASSWORD}`
-  ) {
-    console.log('successful login');
-    const { data } = await axios.get(
-      `https://issues.redhat.com/rest/api/2/issue/THEEDGE-${id}`,
-      {
-        auth: {
-          username: `${process.env.JIRA_USERNAME}`,
-          password: `${process.env.JIRA_PASSWORD}`,
-        },
-      }
-    );
-    return res.json({
-      title: data.fields.summary,
-      description: data.fields.description,
-      assignee: data.fields.assignee.name,
-      link: `https://issues.redhat.com/browse/THEEDGE-${id}`,
-    });
-  }
-  return res.status(401).json({ message: 'You are not authorized' });
-});
-
-app.post('/slack/actions', async (req, res) => {
-  console.log('it works!');
-  try {
-    const payload = JSON.parse(req.body.payload);
-    console.log('###slack request is ' + payload);
-
-    return res.send(response);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send('Something went wrong.');
-  }
-});
-
-dotenv.config();
-
-const bot = new SlackBot({
-  token: `${process.env.BOT_TOKEN}`,
-  name: 'storypointbot',
 });
 
 // Start Handler
@@ -78,18 +37,70 @@ bot.on('message', (data) => {
     return;
   }
   console.log('received message');
-  console.log(data.text.split(' ')[1]);
-
-  handleTestMessage(data);
+  getTicketById(data);
 });
 
-const handleTestMessage = (data) => {
-  bot.getUserById(data.user).then((userData) => {
-    console.log(userData);
-    bot.postMessageToChannel(
-      'random',
-      `@${userData.real_name} said ${data.text.split(' ')[1]}`
-      //      customMessage
-    );
-  });
+//bot.getUserById(data.user).then((userData) => {}
+const getTicketById = async (messageData) => {
+  const id = messageData.text.split(' ')[1];
+  const { data } = await axios.get(
+    `https://issues.redhat.com/rest/api/2/issue/THEEDGE-${id}`,
+    {
+      auth: {
+        username: `${process.env.JIRA_USERNAME}`,
+        password: `${process.env.JIRA_PASSWORD}`,
+      },
+    }
+  );
+  const info = {
+    title: data.fields.summary,
+    description: data.fields.description,
+    assignee: data.fields.assignee.name,
+    link: `https://issues.redhat.com/browse/THEEDGE-${id}`,
+  };
+
+  bot.postMessageToChannel(
+    'random',
+    info
+    //      customMessage
+  );
 };
+
+//app.get(`/ticket/${ticketID}`, async (req, res) => {
+//  const { name, pass } = basicAuth(req);
+//  if (
+//    name === `${process.env.STORYPOINT_USERNAME}` &&
+//    pass === `${process.env.STORYPOINT_PASSWORD}`
+//  ) {
+//    console.log('successful login');
+//    const { data } = await axios.get(
+//      `https://issues.redhat.com/rest/api/2/issue/THEEDGE-${id}`,
+//      {
+//        auth: {
+//          username: `${process.env.JIRA_USERNAME}`,
+//          password: `${process.env.JIRA_PASSWORD}`,
+//        },
+//      }
+//    );
+//    return res.json({
+//      title: data.fields.summary,
+//      description: data.fields.description,
+//      assignee: data.fields.assignee.name,
+//      link: `https://issues.redhat.com/browse/THEEDGE-${id}`,
+//    });
+//  }
+//  return res.status(401).json({ message: 'You are not authorized' });
+//});
+//
+//app.post('/slack/actions', async (req, res) => {
+//  console.log('it works!');
+//  try {
+//    const payload = JSON.parse(req.body.payload);
+//    console.log('###slack request is ' + payload);
+//
+//    return res.send(response);
+//  } catch (err) {
+//    console.log(err);
+//    return res.status(500).send('Something went wrong.');
+//  }
+//});

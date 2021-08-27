d7283b5beb3b6b1fc90e527a74f86ebb049bf70e
csantiago <resolutecoder@gmail.com>
2021-08-27 13:22:14 -0500

    added polling visuals

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const SlackBot = require('slackbots');
 const dotenv = require('dotenv');
-//const customMessage = require('./block.json');
 const axios = require('axios');
-const basicAuth = require('basic-auth');
 
 const express = require('express');
 const app = express();
@@ -32,20 +30,23 @@ bot.on('error', (err) => {
 });
 
 // Message Handler
-bot.on('message', (data) => {
+bot.on('message', async (data) => {
   // check for one bot per ticket
-  if (data.type !== 'message' || data.bot_id) {
-    return;
+  console.log(data);
+  if (data.type !== 'message') return;
+  if (data.text.includes('<@U02BSM1S2RY>')) {
+    console.log('received message', data);
+    const ticketInfo = await getTicketById(149);
+    const params = { icon_emoji: ':robot_face:', blocks: [] };
+    params.blocks.push(...createTicketInfo(ticketInfo));
+    params.blocks.push(...createPoll().flat());
+    bot.postMessageToChannel('test', 't', params);
   }
-
-  console.log('received message', data);
-  getTicketById(data);
+  if (data.bot_id === 'B02BPL9S11T') {
+  }
 });
 
-//bot.getUserById(data.user).then((userData) => {}
-const getTicketById = async (messageData) => {
-  //const id = messageData.text.split(' ')[1];
-  const id = 958;
+const getTicketById = async (id) => {
   try {
     const { data } = await axios.get(
       `https://issues.redhat.com/rest/api/2/issue/THEEDGE-${id}`,
@@ -56,106 +57,104 @@ const getTicketById = async (messageData) => {
         },
       }
     );
-    const info = {
+    return {
       title: data.fields.summary,
       description: data.fields.description,
       assignee: data.fields.assignee.name,
       link: `https://issues.redhat.com/browse/THEEDGE-${id}`,
+      storyPoint: data.fields['customfield_12310243'],
+      id,
     };
-    console.log(createMessage(info));
-    bot.postMessageToChannel(
-      'random',
-      'test',
-      {
-        icon_emoji: ':robot_face:',
-        blocks: [
-          {
-            type: 'header',
-            text: {
-              type: 'plain_text',
-              text: `:ticket: ${info.title}? :ticket:`,
-            },
-          },
-          {
-            type: 'context',
-            elements: [
-              {
-                text: `*Assigned to ${info.assignee}* |  <${info.link}|THEEDGE-${id}>`,
-                type: 'mrkdwn',
-              },
-            ],
-          },
-          {
-            type: 'divider',
-          },
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: '*Description*',
-            },
-          },
-          {
-            type: 'section',
-            text: {
-              type: 'plain_text',
-              text: `${info.description}`,
-            },
-          },
-          {
-            type: 'divider',
-          },
-        ],
-      }
-      //      customMessage
-    );
   } catch (error) {
-    console.log(error);
+    console.log('ERROR', error);
   }
 };
 
-const createMessage = ({ title, description, assignee, link }) => {
-  const msgTitle = {
+const createTicketInfo = ({
+  title,
+  description,
+  assignee,
+  storyPoint,
+  link,
+  id,
+}) => [
+  {
+    type: 'header',
+    text: {
+      type: 'plain_text',
+      text: `:ticket: ${title}? :ticket:`,
+    },
+  },
+  {
+    type: 'context',
+    elements: [
+      {
+        text: `*Assigned to ${assignee}* |  <${link}|THEEDGE-${id}>`,
+        type: 'mrkdwn',
+      },
+    ],
+  },
+  {
+    type: 'divider',
+  },
+  {
     type: 'section',
-    text: { type: 'mrkdown', text: `*${title}?* Assigned to ${assignee}` },
-  };
-  return [msgTitle];
+    text: {
+      type: 'mrkdwn',
+      text: '*Description*',
+    },
+  },
+  {
+    type: 'section',
+    text: {
+      type: 'plain_text',
+      text: `${storyPoint} ${description}`,
+    },
+  },
+  {
+    type: 'divider',
+  },
+];
+
+const createPoll = () => {
+  const nums = [1, 2, 5, 8, 13];
+  return nums.map((num) => [
+    {
+      type: 'actions',
+      elements: [
+        {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: `${num}`,
+            emoji: true,
+          },
+          value: `story_point_${num}`,
+          action_id: `actionId-${num}`,
+        },
+      ],
+    },
+    {
+      type: 'context',
+      elements: [
+        {
+          type: 'mrkdwn',
+          text: 'No votes',
+        },
+      ],
+    },
+  ]);
 };
-//app.get(`/ticket/${ticketID}`, async (req, res) => {
-//  const { name, pass } = basicAuth(req);
-//  if (
-//    name === `${process.env.STORYPOINT_USERNAME}` &&
-//    pass === `${process.env.STORYPOINT_PASSWORD}`
-//  ) {
-//    console.log('successful login');
-//    const { data } = await axios.get(
-//      `https://issues.redhat.com/rest/api/2/issue/THEEDGE-${id}`,
-//      {
-//        auth: {
-//          username: `${process.env.JIRA_USERNAME}`,
-//          password: `${process.env.JIRA_PASSWORD}`,
-//        },
-//      }
-//    );
-//    return res.json({
-//      title: data.fields.summary,
-//      description: data.fields.description,
-//      assignee: data.fields.assignee.name,
-//      link: `https://issues.redhat.com/browse/THEEDGE-${id}`,
-//    });
-//  }
-//  return res.status(401).json({ message: 'You are not authorized' });
-//});
-//
-//app.post('/slack/actions', async (req, res) => {
-//  console.log('it works!');
-//  try {
-//    const payload = JSON.parse(req.body.payload);
-//    console.log('###slack request is ' + payload);
-//
-//    return res.send(response);
-//  } catch (err) {
-//    console.log(err);
-//    return res.status(500).send('Something went wrong.');
-//  }
-//});
+
+app.post('/slack/actions', async (req, res) => {
+  console.log('it works!');
+  try {
+    const payload = JSON.parse(req.body.payload);
+    console.log('###slack request is ' + payload);
+
+    return res.send(response);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('Something went wrong.');
+  }
+});
